38a9d3eb783472389e6a0826ef4d1bd88814b2eb
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-01 00:05:38 +0700

    Добавил вторую версию решения, а именно, использовал другой метод для перемножения цифр

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+// const num = 266219; // создаём переменную
+// const numStr = String(num); // помещаем в переменную число преоразованное в строку
+// const numArr = Array.from(numStr); // помещаем строку в массив, это новый элемент массива
+// let numProd = 1;
+
+// for (let i = 0; i < numArr.length; i++) { //умножаем каждый элемент массива
+//     numProd = numProd * numArr[i];
+// }
+
+// console.log(numProd); //выводим полученное значение
+
+// numProd **= 3; //возводим в 3 степень
+
+// console.log(String(numProd).substring(0, 2)); //выводим первые 2 цифры числа, преобразованного в строку
+
+
+// Версия 1
+
+
+
+
 const num = 266219; // создаём переменную
 const numStr = String(num); // помещаем в переменную число преоразованное в строку
 const numArr = Array.from(numStr); // помещаем строку в массив, это новый элемент массива
-let numProd = 1;
 
-for (let i = 0; i < numArr.length; i++) { //умножаем каждый элемент массива
-    numProd = numProd * numArr[i];
-}
+let numMult = numArr.reduce(function (currentMult, currentNumber) { //Используем reduce для того, чтобы уможить каждую цифру
+    return currentMult * currentNumber
+}, 1);
 
-console.log(numProd); //выводим полученное значение
+console.log(numMult); //выводим полученное значение
 
-numProd **= 3; //возводим в 3 степень
+numMult **= 3; //возводим в 3 степень
 
-console.log(String(numProd).substring(0, 2)); //выводим первые 2 цифры числа, преобразованного в строку
+console.log(String(numMult).substring(0, 2)); //выводим первые 2 цифры числа, преобразованного в строку
+
+//Версия 2
