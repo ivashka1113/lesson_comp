12c9521ebafc6bf165a208308f54b5f5d3c2ac67
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-03 21:04:17 +0700

    Выполнил усложненное задание 3 урока

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-// const num = 266219; // создаём переменную
-// const numStr = String(num); // помещаем в переменную число преоразованное в строку
-// const numArr = Array.from(numStr); // помещаем строку в массив, это новый элемент массива
-// let numProd = 1;
-
-// for (let i = 0; i < numArr.length; i++) { //умножаем каждый элемент массива
-//     numProd = numProd * numArr[i];
-// }
-
-// console.log(numProd); //выводим полученное значение
-
-// numProd **= 3; //возводим в 3 степень
-
-// console.log(String(numProd).substring(0, 2)); //выводим первые 2 цифры числа, преобразованного в строку
+let lang = prompt("Выберите язык/Choose language (ru/en)");
+const langArr = [];
 
 
-// Версия 1
+if (lang === "ru") {
+    console.log("Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье");
+} else {
+    console.log("Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday");
+}
 
+switch (true) {
+    case lang === "ru":
+        console.log("Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье");
+        break;
+    default:
+        console.log("Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday");
+        break;
+}
 
+langArr['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+langArr['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
+console.log(langArr[lang]);
 
-const num = 266219; // создаём переменную
-const numStr = String(num); // помещаем в переменную число преоразованное в строку
-const numArr = Array.from(numStr); // помещаем строку в массив, это новый элемент массива
+let namePerson = prompt("Введите имя (Артём, Александр, др.)");
 
-let numMult = numArr.reduce(function (currentMult, currentNumber) { //Используем reduce для того, чтобы уможить каждую цифру
-    return currentMult * currentNumber
-}, 1);
-
-console.log(numMult); //выводим полученное значение
-
-numMult **= 3; //возводим в 3 степень
-
-console.log(String(numMult).substring(0, 2)); //выводим первые 2 цифры числа, преобразованного в строку
-
-//Версия 2
+namePerson === "Артём" ? console.log(namePerson + " - директор") : namePerson === "Александр" ? console.log(namePerson + " - преподаватель") : console.log(namePerson + " - студент");
