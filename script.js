4a7030e830adb46f472c8f8f3bab81e35d1698e5
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-20 23:46:55 +0700

    Выполнил третье усложненное задание 16 урока

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 "use strict";
 
-function getResult(x, y) {
-    let result;
-    let pow = x ** y;
-    const resultArr = Array.from(String(pow), Number);
-    return result = resultArr.reduce((sum, num) => sum + num);
+const countryInput = document.getElementById("country");
+const cityInput = document.getElementById("city");
+const result = document.querySelector(".result")
+
+const cityArr = {
+    rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
+    uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
+    bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
+    jap: ['Токио', 'Киото', 'Осака', 'Иокогама']
 }
 
-console.log(getResult(4, 8));
+const cityShow = () => {
+    cityInput.style.display = "block";
+    cityInput.innerHTML = "";
+    let countryValue = countryInput.value;
+    cityArr[countryValue].forEach((item) => {
+        const opt = document.createElement('option');
+        opt.innerHTML = `<option>${item}</option>`;
+        cityInput.append(opt);
+    });
+    showResult();
+}
+
+const showResult = () => {
+    result.textContent = `${countryInput.options[countryInput.selectedIndex].textContent} ${cityInput.value}`;
+}
+
+countryInput.addEventListener('change', cityShow);
+cityInput.addEventListener('change', showResult);
