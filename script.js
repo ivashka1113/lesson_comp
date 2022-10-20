eb540a00d79aa3e6a15d3859f662442ed3c948cc
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-20 22:47:33 +0700

    Выполнил второе усложненное задание 16 урока

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
 "use strict";
-const inputA = document.getElementById("a");
-const inputB = document.getElementById("b");
-const sumBtn = document.getElementById("sum");
-const multBtn = document.getElementById("mult");
-const inputRes = document.getElementById("res");
 
-const calculator = {
-    result: 0,
-
-    sum: () => {
-        console.log("Событие");
-        inputA.value === "" || inputA.value === "" ?
-            alert("Вы ввели не все данные") : this.result = +inputA.value + +inputB.value;
-        calculator.show();
-    },
-
-    mult: () => {
-        console.log("Событие");
-        inputA.value === "" || inputA.value === "" ?
-            alert("Вы ввели не все данные") : this.result = +inputA.value * +inputB.value;
-        calculator.show();
-    },
-
-    show: () => {
-        console.log("Событие");
-        inputRes.value = this.result;
-        console.log(this.result);
-        console.log(typeof this.result);
-        if (isNaN(this.result)) alert("Вы ввели не числа");
-        this.result = 0;
-    },
-
-    start: () => {
-        sumBtn.addEventListener("click", calculator.sum);
-        multBtn.addEventListener("click", calculator.mult);
-    }
-
+function getResult(x, y) {
+    let result;
+    let pow = x ** y;
+    const resultArr = Array.from(String(pow), Number);
+    return result = resultArr.reduce((sum, num) => sum + num);
 }
 
-calculator.start();
+console.log(getResult(4, 8));
