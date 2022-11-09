83e5635a0405ba45cb9b9e06d07e747c12dc9360
Ivan Scherbakov <ivashka1113@mail.ru>
2022-11-09 21:43:03 +0700

    Выполнил первое усложненное задание 28 урока

--- script.js
+++ script.js
@@ -1,57 +1,35 @@
 "use strict";
 
-const man = document.getElementById("man");
-const air = document.getElementById("air");
-const btnFly = document.getElementById("animation");
-const btnDump = document.getElementById("animation_1");
-const btnReset = document.getElementById("reset");
-let countFly = 0;
-let countDump = 0;
-let activeFly = false;
-let activeDump = false;
-let flyTime;
-let flyDumpTime;
+const carSelect = document.getElementById("cars");
+const selectedCarInfo = document.querySelector(".selected-car")
 
-
-const flyAir = () => {
-    countFly++;
-    flyTime = requestAnimationFrame(flyAir);
-    countFly < 400 ? air.style.left = countFly * 4 + "px" : cancelAnimationFrame(flyTime);
+const getCars = ({
+    url: url,
+    data: data
+}) => {
+    return fetch(url, {
+        method: "GET",
+        body: data,
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    }).then(data => data.json())
 }
 
-btnFly.addEventListener("click", () => {
-    if (activeFly) {
-        cancelAnimationFrame(flyTime);
-        activeFly = !activeFly;
-    } else {
-        flyTime = requestAnimationFrame(flyAir);
-        activeFly = !activeFly;
-    }
-})
-
-const flyDump = () => {
-    countDump++;
-    flyDumpTime = requestAnimationFrame(flyDump);
-    countDump < 450 ? man.style.top = countDump * 2 + "px" : cancelAnimationFrame(flyDumpTime);
-}
-
-btnDump.addEventListener("click", () => {
-    if (activeDump) {
-        cancelAnimationFrame(flyDumpTime);
-        activeDump = !activeDump;
-    } else {
-        flyDumpTime = requestAnimationFrame(flyDump);
-        activeDump = !activeDump;
-    }
-})
-
-btnReset.addEventListener("click", () => {
-    cancelAnimationFrame(flyTime);
-    cancelAnimationFrame(flyDumpTime);
-    countFly = 0;
-    countDump = 0;
-    activeFly = false;
-    activeDump = false;
-    air.style.left = 0;
-    man.style.top = 0;
+carSelect.addEventListener("change", (data) => {
+    const carSeletcValue = carSelect.options[carSelect.selectedIndex].value;
+    getCars({
+        url: "./cars.json"
+    }).then(data => {
+        data.cars.forEach(car => {
+            if (carSeletcValue === car.brand) {
+                selectedCarInfo.innerHTML = `
+                <p>Тачка: ${car.brand} ${car.model}</p>
+                <p>Цена: ${car.price}$</p>`
+                console.log(carSeletcValue, car.brand)
+            } else if (carSeletcValue === "") {
+                selectedCarInfo.innerHTML = "";
+            };
+        });
+    })
 })
